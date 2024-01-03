f01cc932ecfa06f9676a1130c84c958ad2eceda0
Dan Cordell <dan.i.cordell@gmail.com>
2024-01-03 10:23:49 -0800

    maybe getting this set up correctly now

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 #app SKPyLogger
 
 import restapi, asyncio, logger
+from flask import Flask
+
+app = Flask(__name__)
 
 async def main():
     print("Starting...")
-    await asyncio.gather(restapi.restapi(), logger.periodicLogging())
+    await asyncio.gather(restapi.restapi(app), logger.periodicLogging())
 
 loop = asyncio.new_event_loop()
 loop.run_until_complete(main())
